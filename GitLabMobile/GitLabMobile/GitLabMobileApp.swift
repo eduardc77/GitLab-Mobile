8885bf0f9925c291b1c0279830beb13f1fe7d554
Eduardc77 <36419167+eduardc77@users.noreply.github.com>
2025-08-12 16:25:33 +0300

    Add files via upload

---
+++ GitLabMobile/GitLabMobile/GitLabMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLabMobileApp.swift
+//  GitLabMobile
+//
+//  Created by User on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLabMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
